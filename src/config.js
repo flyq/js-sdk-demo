9f2a5d8bd5cc3b186a07834b317782c598607cf4
flyq <flyq951@gmail.com>
2021-02-25 10:57:06 +0800

    Init

---
+++ src/config.js
@@ -0,0 +1,5 @@
+const config = {
+  chain: 'https://scan.taidihub.com:8888/',
+  privateKey: '0x032aea73ea4aec44af811124433261fbb722bfde60d7bc1454baf1a1cfef30fb'
+}
+module.exports = config
---
+++ src/transaction.js
@@ -0,0 +1,15 @@
+const cita = require("./cita-sdk");
+const transaction = {
+  nonce: 999999,
+  quota: 1000000,
+  chainId: "0x1",
+  version: 2,
+  validUntilBlock: 999999,
+  value: "0x0"
+};
+if (process.env.REACT_APP_RUNTIME === "web" || process.env.REACT_APP_RUNTIME === "webRandomPk") {
+  transaction.from = cita.getFromAddress();
+  transaction.privateKey = cita.base.accounts.wallet[0].privateKey;
+}
+
+module.exports = transaction;
